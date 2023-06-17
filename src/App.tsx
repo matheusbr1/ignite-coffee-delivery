684ad191aaa1aa67558b0c2480dae0b71d437d96
Matheus Baron <matheusbaron10@gmail.com>
2023-06-17 13:36:10 -0300

    feat: create global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/theme'
 import { Products } from './pages/Products'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme} >
       <Products />
+
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
