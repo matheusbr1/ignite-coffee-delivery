4d2add0813c201a3d074a5a27a01d3fc90658b61
Matheus Baron <matheusbaron10@gmail.com>
2023-06-17 13:31:44 -0300

    feat: config eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,7 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "plugin:@rocketseat/eslint-config/react"
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
