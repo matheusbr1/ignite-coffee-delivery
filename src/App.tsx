bf0ec17a4a7b0daa763f35d19a62238f702dd175
Matheus Baron <matheusbaron10@gmail.com>
2023-07-12 22:30:50 -0300

    feat: add react router

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,31 @@ import { Products } from './pages/Products'
 import { Checkout } from './pages/Checkout'
 import { ConfirmedOrder } from './pages/ConfirmedOrder'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Products />
+  },
+  {
+    path: "/checkout",
+    element: <Checkout />
+  },
+  {
+    path: "/order-confirmation",
+    element: <ConfirmedOrder />
+  },
+])
+
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme} >
       <Layout>
-        {/* <Products /> */}
-        <Checkout />
-        {/* <ConfirmedOrder /> */}
+        <RouterProvider router={router} />
       </Layout>
 
       <GlobalStyle />
